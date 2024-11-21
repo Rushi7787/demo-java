94705b2c2c680312b25ae613d94a8909d646dfa9
Rushi7787 <rushikeshmshelke@gmail.com>
2024-11-21 07:02:37 +0000

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 echo "I am form different universe"
-sudo cp /home/ubuntu/demo-java/target/demo.war /home/ubuntu/apache-tomcat-9.0.97/webapps
+sudo cp -r /home/ubuntu/demo-java/target/demo.war /home/ubuntu/apache-tomcat-9.0.97/webapps
 sudo bash /home/ubuntu/apache-tomcat-9.0.97/bin/shutdown.sh
 sudo bash /home/ubuntu/apache-tomcat-9.0.97/bin/startup.sh
 echo "build success"
